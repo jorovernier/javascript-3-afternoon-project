19505233627faf58c9b10bc5da57c0783e42a814
Joely Vernier <55214971+jorovernier@users.noreply.github.com>
2019-09-27 16:26:58 -0700

    Answered all problems

--- nesting.js
+++ nesting.js
@@ -50,7 +50,17 @@ var employees = [
     3. Return the updated employee array.
 */
 
-//Code Here
+function employeeUpdater(){
+  for(var i = 0; i < employees.length; i++){
+    if(employees[i].firstName === 'Theo'){
+      employees.splice(i,1)
+    }else {if (employees[i].firstName === 'Lorie'){
+      employees[i].department = 'HR'
+    }
+    }
+  }
+  return employees;
+}
 
 
 
@@ -68,8 +78,16 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
-
+function removeDuplicates(array){
+  let newArray = [];
+  for (var i = 0; i < array.length; i++){
+      if (!newArray.includes(array[i])){
+        newArray.push(array[i]);
+      }
+    }
+    return newArray;
+  }
+removeDuplicates(workplaceAccidents)
 
 
 ////////// PROBLEM 3 //////////
@@ -97,8 +115,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 
@@ -138,8 +156,14 @@ var myCar = {
     3. Change atFaultForAccident from true to false.
 */
 
-//Code Here
-
+function recordCleaner(){
+  for (var i = 0; i < myCar.accidents.length; i++){
+      if(myCar.accidents[i].atFaultForAccident === true){
+        myCar.accidents[i].atFaultForAccident = false;
+      } 
+    }
+  }
+  console.log(recordCleaner(myCar))
 
 
 ////////// PROBLEM 5 //////////
@@ -157,6 +181,17 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
 
-
+function looper() {
+  for (let i = 0; i < numsArr.length; i++) {
+    for (let j = 0; j < numsArr[i].length; j++) {
+      if (numsArr[i][j] % 2 !== 0) {
+        numsArr[i][j] = 'odd';
+      }
+      else {
+        numsArr[i][j] = 'even';
+      }
+    }
+  }
+  return numsArr;
+}
